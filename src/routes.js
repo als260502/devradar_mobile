e28cbce964bc7c52cb955a53dad336ccb52cf743
Andre <als260602@msn.com>
2020-01-17 10:04:48 -0300

    Projeto dev radar da semana omnistack 10.0 modulo mobile

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Main from "./pages/Main";
+import Profile from "./pages/Profile";
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        title: "DevRadar"
+      },
+      Profile: {
+        screen: Profile,
+        title: "Perfil no Github"
+      }
+    },
+    {
+      defaultNavigationOptions: {
+        headerTintColor: "#fff",
+        headerTitleAlign: "center",
+        headerStyle: {
+          backgroundColor: "#7d40e7"
+        }
+      }
+    }
+  )
+);
+
+export default Routes;
